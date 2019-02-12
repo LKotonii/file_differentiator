ca68d78ed32f872118b1bb2b595d41b17277015d
Liliia Kotonii <l.kotonij@yahoo.com>
2019-02-13 00:30:28 +0100

    file reader class

--- magic_bytes/src/main/java/magic_bytes/read_file/FileReader.java
+++ magic_bytes/src/main/java/magic_bytes/read_file/FileReader.java
@@ -1,5 +1,8 @@
 package magic_bytes.read_file;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,4 +28,44 @@ public class FileReader {
 		}
 		return magicNumbers;
 	}
+	
+	 static SupportedExtension fileExtension(String filename) {
+			char[] chars = filename.toCharArray();
+			StringBuilder extension = new StringBuilder("");
+			// we need to get all chars after the last occurence of the dot in the filename which will be declared file extension
+			for (int i = chars.length - 1; i > 1; i--) {
+				if (chars[i] == '.') {
+					break;
+				} else
+					extension.append(chars[i]);
+			}
+			switch (extension.toString()) {
+			
+			case "fig":
+				return SupportedExtension.GIF;
+			case "txt":
+				return SupportedExtension.TXT;
+			case "gpj":
+				return SupportedExtension.JPG;
+			case "gepj":
+				return SupportedExtension.JPG;
+			}
+			return null;
+		}
+
+		 static String magicNumbersOfFile(Path filePath) {
+			
+			StringBuilder st = new StringBuilder("");
+			try {
+				byte[] bytes = Files.readAllBytes(filePath);
+				for (int i = 0; i < 12; i++) {
+					st.append(String.format("%02X", bytes[i]));
+				}
+				
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return st.toString();
+		}
 }
