b3f4b6ab54d9ce0fe492b6a883a96e3342515688
Liliia Kotonii <l.kotonij@yahoo.com>
2019-02-13 01:50:13 +0100

    main class

--- magic_bytes/src/main/java/magic_bytes/MainClass.java
+++ magic_bytes/src/main/java/magic_bytes/MainClass.java
@@ -1,10 +1,47 @@
 package magic_bytes;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import magic_bytes.exception.ExtensionNotHandledException;
+import magic_bytes.read_file.FileReader;
+import magic_bytes.read_file.FileValidator;
+import magic_bytes.read_file.SupportedExtension;
+
 public class MainClass {
 
-	public static void main(String[] args) {
-		
+	static void readFile(Path path) throws ExtensionNotHandledException {
+		if (FileValidator.extensionIsTrue(path)) {
+			System.out.print("The declared extension and actual extension are same");
+		} else {
+			String s = path.toString();
+			StringBuilder declaredExtension = new StringBuilder("");
+			int dot = s.lastIndexOf('.');
+			for (int i = dot; i < s.length(); i++) {
+				declaredExtension.append(s.charAt(i));
+			}
+			SupportedExtension ext = null;
+			try {
+				ext = FileReader.fileExtension(s);
+			} catch (Exception exc) {
+				exc.getLocalizedMessage();
+			}
+			if (ext != null) {
+				System.out.println("Declared Extension is: " + declaredExtension + ", but real extension is: " + ext);
+			} else {
+				System.out.println("Unhandled extension");
+			}
+		}
+	}
 
+	public static void main(String[] args) {
+
+		try {
+			readFile(Paths.get("/home/rats/git/magic_bytes/src/test/resources/poem.txt.jpg"));
+		} catch (ExtensionNotHandledException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- magic_bytes/src/main/java/magic_bytes/read_file/FileReader.java
+++ magic_bytes/src/main/java/magic_bytes/read_file/FileReader.java
@@ -7,9 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-	
-	// method returns list of all possible hexadecimal signature for SupportedExtension
-	static List<String> defaultMagicNumbers(SupportedExtension fileExtension) {
+
+	// method returns list of all possible hexadecimal signature for
+	// SupportedExtension
+	public static List<String> defaultMagicNumbers(SupportedExtension fileExtension) {
 		ArrayList<String> magicNumbers = new ArrayList<>();
 		switch (fileExtension) {
 		case GIF:
@@ -28,44 +29,44 @@ public class FileReader {
 		}
 		return magicNumbers;
 	}
-	
-	 static SupportedExtension fileExtension(String filename) {
-			char[] chars = filename.toCharArray();
-			StringBuilder extension = new StringBuilder("");
-			// we need to get all chars after the last occurence of the dot in the filename which will be declared file extension
-			for (int i = chars.length - 1; i > 1; i--) {
-				if (chars[i] == '.') {
-					break;
-				} else
-					extension.append(chars[i]);
-			}
-			switch (extension.toString()) {
-			
-			case "fig":
-				return SupportedExtension.GIF;
-			case "txt":
-				return SupportedExtension.TXT;
-			case "gpj":
-				return SupportedExtension.JPG;
-			case "gepj":
-				return SupportedExtension.JPG;
-			}
-			return null;
-		}
 
-		 static String magicNumbersOfFile(Path filePath) {
-			
-			StringBuilder st = new StringBuilder("");
-			try {
-				byte[] bytes = Files.readAllBytes(filePath);
-				for (int i = 0; i < 12; i++) {
-					st.append(String.format("%02X", bytes[i]));
-				}
-				
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return st.toString();
+	public static SupportedExtension fileExtension(String filename) {
+		char[] chars = filename.toCharArray();
+		StringBuilder extension = new StringBuilder("");
+		// we need to get all chars after the last occurence of the dot in the filename
+		// which will be declared file extension
+		for (int i = chars.length - 1; i > 1; i--) {
+			if (chars[i] == '.') {
+				break;
+			} else
+				extension.append(chars[i]);
 		}
+		switch (extension.toString()) {
+
+		case "fig":
+			return SupportedExtension.GIF;
+		case "txt":
+			return SupportedExtension.TXT;
+		case "gpj":
+			return SupportedExtension.JPG;
+		case "gepj":
+			return SupportedExtension.JPG;
+		}
+		return null;
+	}
+
+	public static String magicNumbersOfFile(Path filePath) {
+
+		StringBuilder st = new StringBuilder("");
+		try {
+			byte[] bytes = Files.readAllBytes(filePath);
+			for (int i = 0; i < 12; i++) {
+				st.append(String.format("%02X", bytes[i]));
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return st.toString();
+	}
 }
